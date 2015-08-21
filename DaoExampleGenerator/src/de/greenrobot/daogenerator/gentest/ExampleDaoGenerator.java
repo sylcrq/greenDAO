1efdd754df23766bd3603defb3b2a7e313da1fab
sylcrq <syl272365943@126.com>
2015-08-21 13:58:58 +0800

    *): Fix DaoExampleGenerator运行错误

--- DaoExampleGenerator/src/de/greenrobot/daogenerator/gentest/ExampleDaoGenerator.java
+++ DaoExampleGenerator/src/de/greenrobot/daogenerator/gentest/ExampleDaoGenerator.java
@@ -36,7 +36,7 @@ public class ExampleDaoGenerator {
         addNote(schema);
         addCustomerOrder(schema);
 
-        new DaoGenerator().generateAll(schema, "../DaoExample/src-gen");
+        new DaoGenerator().generateAll(schema, "E:/greenDAO/DaoExample/src-gen");
     }
 
     private static void addNote(Schema schema) {
